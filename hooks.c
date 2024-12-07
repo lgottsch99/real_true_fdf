3751ca0f40dfa148a060a175c88adf6963f00984
Lillian Gottschalk <lgottsch@c2r10s5.42prague.com>
2024-12-07 21:26:13 +0100

    final dec 7

--- hooks.c
+++ hooks.c
@@ -6,7 +6,7 @@
 /*   By: lgottsch <lgottsch@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 15:39:37 by lgottsch          #+#    #+#             */
-/*   Updated: 2024/12/07 18:10:56 by lgottsch         ###   ########.fr       */
+/*   Updated: 2024/12/07 21:24:45 by lgottsch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,12 @@ int	quit_window(t_fdf *big)
 //int is passed by internal event handler
 int	destroy_esc(int keycode, t_fdf *big)
 {
-	ft_printf("esc window\n");
+	ft_printf("esc hook\n");
 	if (keycode == XK_Escape)
+	{
 		free_everything(big);
-	exit(0);
+		return (0);
+	}
+	else
+		return (0);
 }
